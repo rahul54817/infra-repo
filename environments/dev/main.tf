fac945da13dec7d5b11e3ceaac0b572a512d6e69
rahul54817 <kumarrahul9977@gmail.com>
2026-07-07 16:38:12 +0530

    Created Security Group for ECS, RDS, ALB

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -5,3 +5,13 @@ module "vpc" {
   project_name = var.project_name
   environment  = var.environment
 }
+
+module "security_group" {
+
+  source = "../../modules/security-group"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  vpc_id = module.vpc.vpc_id
+}
