332eec1368a606dfef48af5bfa0c14ca8787a6ad
rahul54817 <kumarrahul9977@gmail.com>
2026-07-09 18:33:19 +0530

    created scs service for order service

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -96,3 +96,89 @@ module "alb" {
   public_subnet_ids     = module.vpc.public_subnet_ids
   alb_security_group_id = module.security_group.alb_security_group_id
 }
+
+module "order_service" {
+
+  source = "../../modules/ecs-service"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  service_name = "order-service"
+
+  cluster_id = module.ecs.cluster_id
+
+  execution_role_arn = module.iam.ecs_execution_role_arn
+  task_role_arn      = module.iam.order_task_role_arn
+
+  container_image = "${module.ecr.repository_urls["order-service"]}:latest"
+
+  container_port = 8080
+
+  cpu    = 256
+  memory = 512
+
+  desired_count = 1
+
+  subnet_ids = module.vpc.private_subnet_ids
+
+  security_group_ids = [
+    module.security_group.ecs_security_group_id
+  ]
+
+  assign_public_ip = false
+
+  target_group_arn = module.alb.order_target_group_arn
+
+  aws_region = "ap-south-1"
+
+  environment_variables = [
+
+    {
+      name  = "APP_ENV"
+      value = "dev"
+    },
+
+    {
+      name  = "APP_PORT"
+      value = "8080"
+    },
+
+    {
+      name  = "DB_HOST"
+      value = split(":", module.rds.db_endpoint)[0]
+    },
+
+    {
+      name  = "DB_PORT"
+      value = "5432"
+    },
+
+    {
+      name  = "DB_NAME"
+      value = "order_management"
+    },
+
+    {
+      name  = "DB_USER"
+      value = "dbadmin"
+    },
+
+    {
+      name  = "DB_PASSWORD"
+      value = "postgres"
+    },
+
+    {
+      name  = "AWS_REGION"
+      value = var.aws_region
+    },
+
+    {
+      name  = "SNS_TOPIC_ARN"
+      value = module.sns.topic_arn
+    }
+
+  ]
+
+}
--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -1,3 +1,36 @@
 output "environment" {
   value = var.environment
 }
+
+
+output "alb_dns_name" {
+  value = module.alb.alb_dns_name
+}
+
+output "order_service_url" {
+  value = "http://${module.alb.alb_dns_name}"
+}
+
+output "rds_endpoint" {
+  value = module.rds.db_endpoint
+}
+
+output "sns_topic_arn" {
+  value = module.sns.topic_arn
+}
+
+output "inventory_queue_url" {
+  value = module.sqs.inventory_queue_url
+}
+
+output "notification_queue_url" {
+  value = module.sqs.notification_queue_url
+}
+
+output "cluster_name" {
+  value = module.ecs.cluster_name
+}
+
+output "repository_urls" {
+  value = module.ecr.repository_urls
+}
